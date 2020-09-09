0b57324965472762df12577dfe968997fb241e79
seunghun05 <65702655+seunghun05@users.noreply.github.com>
2020-09-09 15:35:17 +0900

    Add files via upload

---
+++ 배열값이동.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+	int a=0,b=0,c=0;
+	int s[100];
+	
+	
+	scanf("%d",&s[0]); //10
+	c++;
+	
+	s[c-i]=s[c-1-i];
+	
+	
+	scanf("%d",&s[0]); //20
+	c++;
+	
+	s[c-i]=s[c-1-i];
+	s[c-i]=s[c-1-i];
+	
+	
+	scanf("%d",&s[0]); //30
+	c++;
+	
+	s[c-i]=s[c-1-i];
+	s[c-i]=s[c-1-i];
+	s[c-i]=s[c-1-i];
+	    
+    return 0;
+}
